fad8b263b90d64ff35721580ef09d5d817bb355d
lennylemony <anastasia.aizman@gmail.com>
2014-03-13 11:59:29 -0700

    debugging with regular surface, surface is attached to main context

--- app/main/index.js
+++ app/main/index.js
@@ -49,15 +49,20 @@ var titleMod = new Modifier({
 /*--------------WHITE GREEN GRADIENT---------------------------------------------------------------*/
 
 
-// var whiteGreenGradientSurf = new CanvasSurface({
-//   size: [undefined, undefined],
-//   canvasSize: [window.innerWidth*2, window.innerHeight*2],
-//   classes: ['famous-surface']
-// });
+var whiteGreenGradientSurf = new CanvasSurface({
+  size: [undefined, undefined],
+  // canvasSize: [window.innerWidth*2, window.innerHeight*2],
+  classes: ['famous-surface'],
+  content: '<p>HI</p>',
+  properties: {
+    bacgroundColor: 'red'
+  }
+});
 
-// var whiteGreenGradientMod = new Modifier({
-//   transform: Transform.translate(0, 0, 0)
-// });
+var whiteGreenGradientMod = new Modifier({
+  // transform: Transform.translate(0, 0, 12)
+  origin: [0, 0]
+});
 
 
 // var colorCanvas = whiteGreenGradientSurf.getContext('2d');
@@ -65,7 +70,7 @@ var titleMod = new Modifier({
 // if (_isAndroid) {
 //     console.log('HERE')
 
-//   var radial = colorCanvas.createLinearGradient( 
+//   var radial = colorCanvas.createLinearGradient(
 //             300 * 0.5 * 2,    // x0
 //             0,                              // y0
 //             300 * 0.5 * 2,    // x1
@@ -73,14 +78,14 @@ var titleMod = new Modifier({
 //             );
 //   radial.addColorStop(0, "rgba(255, 255, 255, 0)");
 //   radial.addColorStop(1, "rgba(50, 206, 168, 1)");
-          
+
 //   colorCanvas.fillStyle = radial;
 //   colorCanvas.fillRect( 0, 0, window.innerWidth* 2, window.innerHeight* 2 );
 
-//   mainCtx.add(whiteGreenGradientMod).add(whiteGreenGradientSurf); 
+//   mainCtx.add(whiteGreenGradientMod).add(whiteGreenGradientSurf);
 // } else {
 //   console.log('HERE')
-//    radial = colorCanvas.createRadialGradient( 
+//    radial = colorCanvas.createRadialGradient(
 //                   300 * 0.5 * 2,    // x0
 //                   500 * 2,         // y0
 //                   0,   // r0
@@ -96,7 +101,7 @@ var titleMod = new Modifier({
 //   colorCanvas.fillRect( 0, 0, window.innerWidth* 2, window.innerHeight* 2 );
 
 
-//   mainCtx.add(whiteGreenGradientMod).add(whiteGreenGradientSurf); 
+  mainCtx.add(whiteGreenGradientMod).add(whiteGreenGradientSurf);
 // }
 
 
@@ -107,7 +112,7 @@ var titleMod = new Modifier({
 
 function _shadowMod() {
   titleSurf.setProperties({
-    textShadow: '0px 0px ' + this.get()[0] + 'px rgba(0, 49, 86, 1)' 
+    textShadow: '0px 0px ' + this.get()[0] + 'px rgba(0, 49, 86, 1)'
   });
 };
 
@@ -115,13 +120,15 @@ function _playShadow() {
   if (devMode ) {
     titleMod.setOpacity(0, function(){});
     var appView = new AppView();
-    mainCtx.add(appView);        
+    mainCtx.add(appView);
     titleMod.setTransform(Transform.translate(0, 2000, -50));
   } else {
     this.set([1.5, 100, 50], {duration: 1500}, function() {
+
       this.set([2, 100, 50], {duration: 500}, function(){
         this.set([0, 100, 50], {duration: 800}, function() {
           Timer.after(function() {
+      // console.log('HERE')
             // whiteGreenGradientMod.setTransform(Transform.translate(0, 0, 2), {duration: 500}, function() {
  // this.gradient.set([50, 206, 168, 255, 255, 255], {duration: 2000, curve: 'easeInOut'}, function() {})
               Timer.after(function(){
@@ -129,23 +136,23 @@ function _playShadow() {
                 mainCtx.add(appView);
                 titleMod.setTransform(Transform.translate(0, 2000, -50), {duration: 0}, function() {
                   titleMod.setOpacity(0, function() {});
-                });                          
+                });
               }, 20);
             });
           }, 7);
         }.bind(this));
       }.bind(this));
-    }.bind(this));    
+    }.bind(this));
   }
 };
 
 function _isAndroid() {
-  var userAgent = navigator.userAgent.toLowerCase();  
+  var userAgent = navigator.userAgent.toLowerCase();
   return userAgent.indexOf("android") > -1;
 };
 
 
 mainCtx.add(titleMod).add(titleSurf);
 mainCtx.add(greenBackgroundMod).add(greenBackgroundSurf);
-window.Engine.on("prerender", _shadowMod.bind(shadowTransitionable));  
+window.Engine.on("prerender", _shadowMod.bind(shadowTransitionable));
 _playShadow.call(shadowTransitionable);
