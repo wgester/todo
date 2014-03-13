4925b4d6b55f0870fef470d01df09aac066baa22
Kara Erickson <kara.erickson3@gmail.com>
2014-03-12 17:45:49 -0700

    Center animation loads before app

--- app/main/index.js
+++ app/main/index.js
@@ -6,6 +6,7 @@ var Transform     = require('famous/transform');
 var Transitionable = require("famous/transitions/transitionable");
 var WallTransition = require("famous/transitions/wall-transition");
 
+
 Transitionable.registerMethod('wall', WallTransition);
 
 var mainCtx = window.Engine.createContext();
@@ -14,17 +15,16 @@ mainCtx.setPerspective(1000);
 
 var shadowTransitionable = new Transitionable([50, 206, 168, 255, 255, 255]);
 
-// _playShadow();
-
 var titleSurf = new Surface({
   size: [undefined, undefined],
+  classes: ['title'],
   content: '<h1>FOCUS</h1>',
   properties: {
-    backgroundColor: '#32CEA8',
-    color: 'transparent'
+    backgroundColor: '#32CEA8'
   }
 });
 
+var titleMod = new Modifier()
 function _shadowMod() {
   titleSurf.setProperties({
     textShadow: '0px 0px ' + this.get()[0] + 'px rgba(0, 49, 86, 1)' 
@@ -36,12 +36,13 @@ function _playShadow() {
     this.set([2, 100, 50], {duration: 800}, function(){
       this.set([0, 100, 50], {duration: 500}, function() {
         var appView = new AppView();
-        mainCtx.add(appView);
-      });
-    });
-  });
+        mainCtx.add(appView);        
+        titleMod.setTransform(Transform.translate(0, 0, -50000));
+      }.bind(this));
+    }.bind(this));
+  }.bind(this));
 };
 
-mainCtx.add(titleSurf);
+mainCtx.add(titleMod).add(titleSurf);
 window.Engine.on("prerender", _shadowMod.bind(shadowTransitionable));  
 _playShadow.call(shadowTransitionable);
