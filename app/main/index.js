689cd58873fb7e9de6bb2f6f8f3fa3cd86f0555f
lennylemony <anastasia.aizman@gmail.com>
2014-03-13 11:41:44 -0700

    refactoring title animation to three surfaces (title, background, gradient)

--- app/main/index.js
+++ app/main/index.js
@@ -18,64 +18,86 @@ var mainCtx = window.Engine.createContext();
 
 mainCtx.setPerspective(1000);
 
-var shadowTransitionable = new Transitionable([50, 206, 168, 255, 255, 255]);
+var shadowTransitionable = new Transitionable([50, 206, 168]);
 
-var titleSurf = new Surface({
+/*--------------GREEN BACKGROUND---------------------------------------------------------*/
+
+var greenBackgroundSurf = new Surface({
   size: [undefined, undefined],
-  classes: ['title'],
-  content: '<h1>FOCUS</h1>',
   properties: {
     backgroundColor: '#32CEA8',
     paddingLeft: 0
   }
 });
 
-var whiteGradientSurf = new CanvasSurface({
+var greenBackgroundMod = new Modifier({
+  transform: Transform.translate(0, 0, 0)
+});
+
+/*--------------BLUR TITLE---------------------------------------------------------------*/
+
+var titleSurf = new Surface({
   size: [undefined, undefined],
-  canvasSize: [window.innerWidth*2, window.innerHeight*2],
-  classes: ['famous-surface']
+  classes: ['title'],
+  content: '<h1>FOCUS</h1>'
 });
 
-var whiteGradientMod = new Modifier({
-  transform: Transform.translate(0, 600, 0)
+var titleMod = new Modifier({
+  transform: Transform.translate(0, 0, 2)
 });
 
+/*--------------WHITE GREEN GRADIENT---------------------------------------------------------------*/
 
-var colorCanvas = whiteGradientSurf.getContext('2d');
 
-if (_isAndroid) {
-  var radial = colorCanvas.createLinearGradient( 
-            300 * 0.5 * 2,    // x0
-            0,                              // y0
-            300 * 0.5 * 2,    // x1
-            500 * 2.5         // y1
-            );
-  
-  radial.addColorStop(0, "rgba(255, 255, 255, 0)");
-  radial.addColorStop(1, "rgba(255, 255, 255, 1)");
+// var whiteGreenGradientSurf = new CanvasSurface({
+//   size: [undefined, undefined],
+//   canvasSize: [window.innerWidth*2, window.innerHeight*2],
+//   classes: ['famous-surface']
+// });
+
+// var whiteGreenGradientMod = new Modifier({
+//   transform: Transform.translate(0, 0, 0)
+// });
+
+
+// var colorCanvas = whiteGreenGradientSurf.getContext('2d');
+
+// if (_isAndroid) {
+//     console.log('HERE')
+
+//   var radial = colorCanvas.createLinearGradient( 
+//             300 * 0.5 * 2,    // x0
+//             0,                              // y0
+//             300 * 0.5 * 2,    // x1
+//             500 * 2.5         // y1
+//             );
+//   radial.addColorStop(0, "rgba(255, 255, 255, 0)");
+//   radial.addColorStop(1, "rgba(50, 206, 168, 1)");
           
-  colorCanvas.fillStyle = radial;
-  colorCanvas.fillRect( 0, 0, window.innerWidth* 2, window.innerHeight* 2 );
-  mainCtx.add(whiteGradientMod).add(whiteGradientSurf);
-} else {
-   radial = colorCanvas.createRadialGradient( 
-                  300 * 0.5 * 2,    // x0
-                  500 * 2,         // y0
-                  0,   // r0
+//   colorCanvas.fillStyle = radial;
+//   colorCanvas.fillRect( 0, 0, window.innerWidth* 2, window.innerHeight* 2 );
 
-                  300 * 0.5 * 2,    // x1
-                  500 * 2.5,       // y1
-                  300 * 2.5        // r1
-                  );
-  radial.addColorStop(0, "rgba(255, 255, 255, 1)");
-  radial.addColorStop(1, "rgba(255, 255, 255, 0)");
+//   mainCtx.add(whiteGreenGradientMod).add(whiteGreenGradientSurf); 
+// } else {
+//   console.log('HERE')
+//    radial = colorCanvas.createRadialGradient( 
+//                   300 * 0.5 * 2,    // x0
+//                   500 * 2,         // y0
+//                   0,   // r0
 
-  colorCanvas.fillStyle = radial;
-  colorCanvas.fillRect( 0, 0, window.innerWidth* 2, window.innerHeight* 2 );
+//                   300 * 0.5 * 2,    // x1
+//                   500 * 2.5,       // y1
+//                   300 * 2.5        // r1
+//                   );
+//   radial.addColorStop(0, "rgba(50, 206, 168, 1)");
+//   radial.addColorStop(1, "rgba(255, 255, 255, 0)");
+
+//   colorCanvas.fillStyle = radial;
+//   colorCanvas.fillRect( 0, 0, window.innerWidth* 2, window.innerHeight* 2 );
 
 
-  mainCtx.add(whiteGradientMod).add(whiteGradientSurf); 
-}
+//   mainCtx.add(whiteGreenGradientMod).add(whiteGreenGradientSurf); 
+// }
 
 
 
@@ -94,13 +116,14 @@ function _playShadow() {
     titleMod.setOpacity(0, function(){});
     var appView = new AppView();
     mainCtx.add(appView);        
-    titleMod.setTransform(Transform.translate(0, 0, -100));
+    titleMod.setTransform(Transform.translate(0, 2000, -50));
   } else {
     this.set([1.5, 100, 50], {duration: 1500}, function() {
       this.set([2, 100, 50], {duration: 500}, function(){
         this.set([0, 100, 50], {duration: 800}, function() {
           Timer.after(function() {
-            whiteGradientMod.setTransform(Transform.translate(0, 100, 0), {duration: 500}, function() {
+            // whiteGreenGradientMod.setTransform(Transform.translate(0, 0, 2), {duration: 500}, function() {
+ // this.gradient.set([50, 206, 168, 255, 255, 255], {duration: 2000, curve: 'easeInOut'}, function() {})
               Timer.after(function(){
                 var appView = new AppView();
                 mainCtx.add(appView);
@@ -123,5 +146,6 @@ function _isAndroid() {
 
 
 mainCtx.add(titleMod).add(titleSurf);
+mainCtx.add(greenBackgroundMod).add(greenBackgroundSurf);
 window.Engine.on("prerender", _shadowMod.bind(shadowTransitionable));  
 _playShadow.call(shadowTransitionable);
