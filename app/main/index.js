a5cbc268f93bfe202c967d7d4faabb5bd05c9782
Kara Erickson <kara.erickson3@gmail.com>
2014-03-12 17:31:52 -0700

    Animation before page load

--- app/main/index.js
+++ app/main/index.js
@@ -10,7 +10,38 @@ Transitionable.registerMethod('wall', WallTransition);
 
 var mainCtx = window.Engine.createContext();
 
-var appView = new AppView();
+mainCtx.setPerspective(1000);
 
-mainCtx.add(appView);
-mainCtx.setPerspective(500);
+var shadowTransitionable = new Transitionable([50, 206, 168, 255, 255, 255]);
+
+// _playShadow();
+
+var titleSurf = new Surface({
+  size: [undefined, undefined],
+  content: '<h1>FOCUS</h1>',
+  properties: {
+    backgroundColor: '#32CEA8',
+    color: 'transparent'
+  }
+});
+
+function _shadowMod() {
+  titleSurf.setProperties({
+    textShadow: '0px 0px ' + this.get()[0] + 'px rgba(0, 49, 86, 1)' 
+  });
+};
+
+function _playShadow() {
+  this.set([1.5, 100, 50], {duration: 1500}, function() {
+    this.set([2, 100, 50], {duration: 800}, function(){
+      this.set([0, 100, 50], {duration: 500}, function() {
+        var appView = new AppView();
+        mainCtx.add(appView);
+      });
+    });
+  });
+};
+
+mainCtx.add(titleSurf);
+window.Engine.on("prerender", _shadowMod.bind(shadowTransitionable));  
+_playShadow.call(shadowTransitionable);
--- app/main/views/HeaderView.js
+++ app/main/views/HeaderView.js
@@ -7,13 +7,9 @@ var Transitionable    = require('famous/transitions/transitionable');
 
 function HeaderView() {
   View.apply(this, arguments);
-  this.shadowTransitionable = new Transitionable([60, 100, 50]);
 
   _createTitle.call(this);
   _buttonListener.call(this);
-  _setListeners.call(this);
-  this.options.title === 'FOCUS' && _playShadow.call(this);    
-  
 }
 
 HeaderView.prototype = Object.create(View.prototype);
@@ -24,21 +20,6 @@ HeaderView.DEFAULT_OPTIONS = {
   classes: ['title'],
   title: 'LATER'
 };
-
-function _shadowMod() {
-  this.titleHeader.setProperties({
-    textShadow: '0px 0px ' + this.shadowTransitionable.get()[0] + 'px rgba(0, 49, 86, 1)' 
-  });
-};
-
-function _playShadow() {
-  this.shadowTransitionable.set([1.5, 100, 50], {duration: 1500}, function() {
-    this.shadowTransitionable.set([2, 100, 50], {duration: 800}, function(){
-      this.shadowTransitionable.set([0, 100, 50], {duration: 500}, function() {});
-    }.bind(this));
-  }.bind(this));
-};
-
 
 function _isAndroid() {
   var userAgent = navigator.userAgent.toLowerCase();  
@@ -58,10 +39,9 @@ function _createTitle() {
     this.titleHeader = new Surface({
       content: '<h1>' + this.options.title + '</h1>',
       properties: {
-        color: 'transparent',
         backgroundColor: new Color('#32CEA8').setLightness(50).getHex()
       }
-    });
+    });        
   } else if (this.options.title === "LATER") {
     this.titleHeader = new Surface({
       content: '<h1>' + this.options.title + '</h1>',
@@ -69,7 +49,7 @@ function _createTitle() {
         backgroundColor: new Color('#9C7CCB').setLightness(65).getHex()
       }
     });
-    this._add(this.titleHeader);      
+
   } else {
     this.titleHeader = new Surface({
       content: '<h1>' + this.options.title + '</h1>',
@@ -77,21 +57,15 @@ function _createTitle() {
         backgroundColor: new Color('#32CEA8').setLightness(50).getHex()
       }
     });
+
   }
   this._add(this.titleHeader);      
-  
 };
 
 function _buttonListener() {
   this.titleHeader.on('touchend', function() {
     this._eventOutput.emit('togglePageViewDown');
   }.bind(this));
-};
-
-function _setListeners() {
-  if (this.options.title === 'FOCUS') {
-    window.Engine.on("prerender", _shadowMod.bind(this));  
-  }
 };
 
 module.exports = HeaderView;
