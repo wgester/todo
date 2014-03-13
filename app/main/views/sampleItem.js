cb54cb3896a30503384d2206571a349766d2d090
Will Gester <willgester@gmail.com>
2014-03-12 20:32:26 -0700

    fixed item properties

--- app/main/views/sampleItem.js
+++ app/main/views/sampleItem.js
@@ -18,9 +18,12 @@ function ClearItem(options) {
     this.surface.pipe(this._eventInput);
     this._eventInput.pipe(this._eventOutput);
 
-    this.name = 'be awesome';
+    this.color   = new Transitionable([0, 0, 0]);
 
-    this.surface.setContent('<p>' + options.text + '</p>');
+    this.name = (this.options.index + 1) + '';
+
+    this.setColor(this.options.index);
+    this.surface.setContent((this.options.index + 1) + '');
 
     bindEvents.call(this);
 
@@ -41,9 +44,19 @@ function ClearItem(options) {
 ClearItem.DEFAULT_OPTIONS = {
     index: 0,
     surface: {
-        classes: ['task'],
-        size: [undefined, 60],
+        size: [undefined, window.innerHeight * 0.14],
         properties: {
+            color: 'white',
+            fontSize: '18px',
+            textAlign: 'center',
+            verticalAlign: 'middle',
+            lineHeight: (window.innerHeight * 0.14) + 'px',
+            borderTopStyle: 'solid',
+            borderTopWidth: '1px',
+            borderBottomStyle: 'solid',
+            borderBottomWidth: '1px',
+            fontWeight: 'bold',
+            fontFamily: 'Helvetica',
             webkitUserSelect: 'none'
         }
     }
@@ -54,6 +67,7 @@ function bindEvents() {
     this._eventInput.on('touchend', handleEnd.bind(this));
     Engine.on('prerender', findTimeDeltas.bind(this));
     Engine.on('prerender', checkForDragging.bind(this));
+    Engine.on('prerender', applyColor.bind(this));
 }
 
 function handleStart() {
@@ -85,6 +99,19 @@ function checkForDragging(data) {
     }
 }
 
+function applyColor() {
+    var main = this.color.get();
+
+    var topColor = 'hsl(' + main[0] + ','  + main[1] + '%,' + (main[2] + 5) + '%)';
+    var mainColor = 'hsl(' + main[0] + ','  + main[1] + '%,' + main[2] + '%)';
+    var bottomColor = 'hsl(' + main[0] + ','  + main[1] + '%,' + (main[2] - 5) + '%)';
+
+    this.surface.setProperties({
+        background: mainColor,
+        borderTopColor: topColor,
+        borderBottomColor: bottomColor
+    });
+}
 
 function dragmode() {
     this.CRAZYmodifier.setTransform(Matrix.translate(0, 0, 40), {
@@ -113,5 +140,13 @@ function regularmode() {
 }
 
 ClearItem.prototype = Object.create(View.prototype);
+
+ClearItem.prototype.setColor = function(index) {
+    var mainColor = ColorMap[index];
+
+    this.color.set(mainColor.getHSL(), {
+        duration: 500
+    });
+};
 
 module.exports = ClearItem;
