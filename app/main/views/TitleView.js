24a6bf58710977519c0552ee167e8dbe1fc10fba
Kara Erickson <kara.erickson3@gmail.com>
2014-03-22 20:55:45 -0700

    Created loading spinner

--- app/main/views/TitleView.js
+++ app/main/views/TitleView.js
@@ -5,12 +5,15 @@ var Transform         = require('famous/transform');
 var Transitionable    = require("famous/transitions/transitionable");
 var Timer             = require('famous/utilities/timer');
 var AppView           = require('./views/AppView');
+var ImageSurface      = require('famous/surfaces/image-surface');
 
 function TitleView(options) {
   View.apply(this, arguments);
   this.shadowTransitionable = new Transitionable([50, 206, 168, 255, 255, 255]);
 
   _createTitleSurface.call(this);
+  _createSpinner.call(this);
+  _createButtons.call(this);
   _setListeners.call(this);
   _playShadow.call(this);
 };
@@ -49,11 +52,53 @@ function _shadowMod() {
 };
 
 function _createButtons() {
+  this.syncButton = new Surface({
+    size: [undefined, 100],
+    content: '<button>Sync with Asana</button>',
+    properties: {
+      textAlign: 'center',
+    }
+  });
+
+  this.skipButton = new Surface({
+    size: [undefined, 100],
+    content: '<button>Skip</button>',
+    properties: {
+      textAlign: 'center'      
+    }
+  });
+
+  this.syncButtonMod = new Modifier({
+    origin: [0, 0.6],
+    opacity: 0
+  });
+
+  this.skipButtonMod = new Modifier({
+    origin: [0, 0.7],
+    opacity: 0
+  });
   
+  this._add(this.syncButtonMod).add(this.syncButton);
+  this._add(this.skipButtonMod).add(this.skipButton);
+};
+
+function _hideButtons() {
+  this.syncButtonMod.setOpacity(0);
+  this.skipButtonMod.setOpacity(0);  
 };
 
 function _setListeners() {
   window.Engine.on("prerender", _shadowMod.bind(this));
+  
+  this.syncButton.on('touchend', function() {
+    _hideButtons.call(this);
+    _populateAsana.call(this);
+  }.bind(this));
+  
+  this.skipButton.on('touchend', function() {
+    _hideButtons.call(this);
+    _createAppView.call(this);
+  }.bind(this));
 };
 
 function _createAppView() {
@@ -65,19 +110,20 @@ function _createAppView() {
 
 function _playShadow() {
   if (this.options.devMode) {
-    this.titleMod.setOpacity(0, function(){});
-    if (this.options.asana && window.localStorage._authKey === undefined) {
-      _populateAsana.call(this);       
-    } else {
-      _createAppView.call(this);
-    }
+    this.titleMod.setOpacity(0);
+     _createAppView.call(this); 
   } else {
     this.shadowTransitionable.set([1.5, 100, 50], {duration: 1500}, function() {
       this.shadowTransitionable.set([2, 100, 50], {duration: 500}, function(){
         this.shadowTransitionable.set([0, 100, 50], {duration: 800}, function() {
           Timer.after(function() {
-            _createAppView.call(this);
-            this.titleMod.setOpacity(0.01, {duration: 500}, function() {});
+            if (this.options.asana && window.localStorage._authKey === undefined) {
+              this.syncButtonMod.setOpacity(1);
+              this.skipButtonMod.setOpacity(1);
+            } else {
+              _createAppView.call(this);
+              this.titleMod.setOpacity(0.01, {duration: 500}, function() {});
+            }
           }.bind(this), 10);
         }.bind(this));
       }.bind(this));
@@ -88,34 +134,62 @@ function _playShadow() {
 function _populateAsana() {
   var APIKey = prompt("What is your api key?");
   window.localStorage._authKey = btoa(APIKey + ":");
-  _getWorkspaces.call(this);  
+  _getWorkspaces.call(null, this);  
 };
 
-function _getWorkspaces() {
+function _createSpinner() {
+  this.spinner = new ImageSurface({
+    size: [36, 36],
+    properties: {
+      display: 'none'
+    }
+  });
+  
+  this.spinner.setContent('./img/spinner2.gif');
+  
+  this.spinnerMod = new Modifier({
+    origin: [0.5, 0.5]
+  });
+  
+  this._add(this.spinnerMod).add(this.spinner);
+};
+
+function _loadSpinner() {
+  this.spinner.setProperties({'display': 'block'});
+};
+
+function _closeSpinner() {
+  this.spinner.setProperties({'display': 'none'});
+};
+
+function _getWorkspaces(context) {
   $.ajax({
     method: 'GET',
     url: 'https://app.asana.com/api/1.0/users/me',
     context: this,
     beforeSend: function(xhr) {
+      _loadSpinner.call(context);
       xhr.setRequestHeader("Authorization", "Basic " + window.localStorage._authKey);
     },
     success: function(resp) {
-      _getTasksFromWorkspaces.call(this, resp.data.workspaces, 0, this);
+      _getTasksFromWorkspaces.call(context, resp.data.workspaces, 0, context);
     },
     error: function(err) {
       alert('Not a valid API key');
-      _createAppView.call(this);
+      _createAppView.call(context);
     }
   }); 
 }; 
 
 function _getTasksFromWorkspaces(spaces, counter, context) {
-  
   $.ajax({
     method: 'GET',
     url: 'https://app.asana.com/api/1.0/workspaces/' + spaces[counter]['id'] + '/tasks?assignee=me&completed_since=now',
     beforeSend: function(xhr) {
       xhr.setRequestHeader("Authorization", "Basic " + window.localStorage._authKey);
+    },
+    complete: function() {
+      _closeSpinner.call(context);
     },
     success: function(resp) {
       for (var i = 0; i < resp.data.length; i++) {
